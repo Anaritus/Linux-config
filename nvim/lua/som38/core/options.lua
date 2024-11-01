cb98df9435f3054fcb57d4abbbb0c40263dc132d
Anaritus <spe4pg@gmail.com>
2024-11-02 02:21:30 +0300

    fix: mostly identation but also changed cursor

--- nvim/lua/som38/core/options.lua
+++ nvim/lua/som38/core/options.lua
@@ -1,53 +1,53 @@
 local M = {}
 
 M.setup = function()
-	local opt = vim.opt -- for conciseness
+  local opt = vim.opt -- for conciseness
 
-	-- yql/arcadia pog
-	vim.g.sql_type_default = "yql"
+  -- yql/arcadia pog
+  vim.g.sql_type_default = "yql"
 
-	-- default terminal
-	vim.g.terminal_emulator = "/bin/zsh"
+  -- default terminal
+  vim.g.terminal_emulator = "/bin/zsh"
 
-	-- line numbers
-	opt.relativenumber = true
-	opt.number = true
-	opt.showmode = true
+  -- line numbers
+  opt.relativenumber = true
+  opt.number = true
+  opt.showmode = true
 
-	opt.guicursor = "n-v-c-i:block-Cursor"
-	-- tabs & identation
-	opt.tabstop = 2
-	opt.shiftwidth = 2
-	opt.expandtab = true
-	opt.autoindent = true
+  -- opt.guicursor = "n-v-c-i:block-Cursor"
+  -- tabs & identation
+  opt.tabstop = 2
+  opt.shiftwidth = 2
+  opt.expandtab = true
+  opt.autoindent = true
 
-	-- line wrapping
-	opt.wrap = false
-	opt.colorcolumn = "80"
+  -- line wrapping
+  opt.wrap = false
+  opt.colorcolumn = "80"
 
-	-- search settings
-	opt.ignorecase = true
-	opt.smartcase = true
+  -- search settings
+  opt.ignorecase = true
+  opt.smartcase = true
 
-	-- cursor line
-	opt.cursorline = true
+  -- cursor line
+  opt.cursorline = true
 
-	-- appearance
-	opt.termguicolors = true
-	opt.background = "dark"
-	opt.signcolumn = "yes"
+  -- appearance
+  opt.termguicolors = true
+  opt.background = "dark"
+  opt.signcolumn = "yes"
 
-	-- backspace
-	opt.backspace = "indent,eol,start"
+  -- backspace
+  opt.backspace = "indent,eol,start"
 
-	-- clipboard
-	opt.clipboard:append("unnamedplus")
+  -- clipboard
+  opt.clipboard:append("unnamedplus")
 
-	-- split windows
-	opt.splitright = true
-	opt.splitbelow = true
+  -- split windows
+  opt.splitright = true
+  opt.splitbelow = true
 
-	opt.iskeyword:append("-")
+  opt.iskeyword:append("-")
 end
 
 return M
